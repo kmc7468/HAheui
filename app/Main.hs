d6dc44fda4dcb32694d5dfeafc8f79498d1c69f3
static <kmc7468@naver.com>
2018-09-16 00:49:06 +0900

    [parse/check]CommandLine 함수 추가
    이 함수들은 각각 명령줄 옵션을 파싱, 파싱된 데이터가 올바른지 검사합니다.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import HAheui.CommandLine as HAheui
+
+import System.Environment (getArgs)
+import System.Exit (exitSuccess)
 
 main :: IO ()
-main = someFunc
+main = do
+    -- Parsing command lines
+    parsedData <- HAheui.parseCommandLine <$> getArgs
+    if not $ HAheui.isDone parsedData
+        then do
+            putStrLn (HAheui.output parsedData)
+            exitSuccess
+        else do
+            let checkedData = HAheui.checkCommandLine parsedData
+            if not $ HAheui.isDone checkedData
+                then do
+                    putStrLn (HAheui.output checkedData)
+                    exitSuccess
+                else return ()
+    putStrLn "Hello, world!"
